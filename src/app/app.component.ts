0dad9a8263e01e43917aecdacf21fb62d2b828a7
czas <zaszas88@gmail.com>
2024-03-07 15:18:19 +0100

    Cambios básicos sobre los componentes añadidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lab-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ActivityBookings';
-}
+export class AppComponent {}
