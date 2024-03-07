7f53bca54bacc4e6de6b98b4a6a984db14456878
czas <zaszas88@gmail.com>
2024-03-07 17:08:38 +0100

    Booking Component. Ejemplo entrada de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingsComponent } from './bookings/bookings.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookingsComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ACTIVITIES } from '../shared/models/activities.data';
+import { Activity } from '../shared/models/activity.type';
+
+@Component({
+  selector: 'lab-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.scss'],
+})
+export class BookingsComponent {
+  public activity: Activity = ACTIVITIES[3];
+
+  public currentParticipants: number = 2;
+
+  public newParticipants: number = 1;
+
+  public totalParticipants: number = this.currentParticipants + this.newParticipants;
+
+  public maxNewParticipants: number =
+    this.activity.maxParticipants - this.currentParticipants;
+
+  public booked: boolean = false;
+
+  public getDisableBookingButton(): boolean {
+    return this.booked || this.newParticipants === 0;
+  }
+
+  public onNewParticipantsChange(event: any) {
+    const input: HTMLInputElement = event.target;
+    const value = input.value;
+    this.newParticipants = parseInt(value, 10);
+    this.totalParticipants = this.currentParticipants + this.newParticipants;
+  }
+
+  public onBookClick() {
+    this.booked = true;
+    if (this.totalParticipants === this.activity.maxParticipants) {
+      this.activity.status = 'sold-out';
+      return;
+    }
+
+    if (this.totalParticipants > this.activity.minParticipants) {
+      this.activity.status = 'confirmed';
+      return;
+    }
+  }
+}
