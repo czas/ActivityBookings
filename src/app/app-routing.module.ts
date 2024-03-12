0d1f055d21e9e374d1d007e90f3ed34df40e1afe
czas <zaszas88@gmail.com>
2024-03-12 17:16:18 +0100

    Routing - basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingsComponent } from './bookings/bookings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BookingsComponent,
+  },
+  {
+    path: 'auth/login',
+    loadChildren: () =>
+      import('./views/auth/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'auth/register',
+    loadChildren: () =>
+      import('./views/auth/register/register.module').then((m) => m.RegisterModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
